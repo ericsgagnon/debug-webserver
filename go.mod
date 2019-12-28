c429cc069d1eb0912d9a02266986e71a2d2d8d9e
Eric <ericsgagnon@gmail.com>
2019-12-28 03:47:14 -0500

    WIP

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/satori/go.uuid v1.2.0
 )
--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 
 	// "github.com/julienschmidt/httprouter"
 	"github.com/gorilla/mux"
+	// uuid "github.com/satori/go.uuid"
 )
 
 type server struct {
@@ -21,13 +20,12 @@ func main() {
 	s.r = mux.NewRouter()
 	s.routes()
 
-	testCrap()
-
 	log.Fatal(http.ListenAndServe(":18080", s.r))
 }
 
 func (s *server) routes() {
-	s.r.HandleFunc("/", s.index())
+	// s.r.HandleFunc(".*", s.index())
+	s.r.PathPrefix("/").HandlerFunc(s.index())
 }
 
 func (s *server) index() http.HandlerFunc {
@@ -39,27 +37,62 @@ func (s *server) index() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprint(w, r.Header)
 
-		data := struct {
-			Headers http.Header
-			test    string
-		}{
-			Headers: r.Header,
-			test:    "stupid",
-		}
+		// c, err := r.Cookie("session")
+		// if err != nil {
+		// 	u := uuid.NewV4()
+		// 	c = &http.Cookie{
+		// 		Name:  "session",
+		// 		Value: u.String(),
+		// 	}
+		// 	http.SetCookie(w, c)
+		// }
 
-		fmt.Fprint(os.Stdout, data)
+		// data := struct {
+		// 	Headers   http.Header
+		// 	Cookies   []*http.Cookie
+		// 	Context   context.Context
+		// 	Request   *http.Request
+		// 	SessionID string
+		// }{
+		// 	Headers:   r.Header,
+		// 	Cookies:   r.Cookies(),
+		// 	Context:   r.Context(),
+		// 	Request:   r,
+		// 	SessionID: c.String(),
+		// }
+
+		// // fmt.Println(reflect.TypeOf(data))
+
+		// dt := reflect.TypeOf(*data.Request)
+		// dv := reflect.ValueOf(*data.Request)
+
+		// // v := make([]interface{}, dv.NumField())
+		// v := make(map[string]interface{})
+
+		// for i := 0; i < dt.NumField(); i++ {
+		// 	fieldName := dv.Type().Field(i).Name
+		// 	if dv.Field(i).CanInterface() {
+		// 		v[fieldName] = dv.Field(i).Interface()
+		// 	} else {
+		// 		v[fieldName] = dv.Field(i).Type()
+		// 	}
+		// 	fmt.Printf("%#v\t%#v\n", fieldName, v[fieldName])
+		// }
+		// fmt.Println(v)
+
+		// fmt.Println(v)
+
+		// fmt.Fprint(w, "Request:\n", r)
+		// fmt.Fprint(os.Stdout, data)
 		// fmt.Fprint(w, r.Cookies())
 		// fmt.Fprint(w, r.Context())
 		// fmt.Fprint(w, r)
 
-		err = t.Execute(w, nil)
+		// err = t.Execute(w, data)
+		err = t.Execute(w, r)
 		if err != nil {
 			// log.Fatalf("Template %#v failed execution using data %#v", t, nil)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
-}
-
-func testCrap() {
-	fmt.Fprintf(os.Stdout, "Test")
 }
